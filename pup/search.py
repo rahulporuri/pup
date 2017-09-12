f49fe241c9fa8339486f91075492353ddea4e941
Poruri Sai Rahul <rporuri@enthought.com>
2017-09-11 21:24:54 -0500

    ADD : use traits/ui to show results in GUI

--- pup/search.py
+++ pup/search.py
@@ -1,7 +1,25 @@
-from __future__ import print_function
-
 from pip import parseopts
 from pip.commands.search import SearchCommand
+
+from traits.api import HasTraits, List, Str
+from traitsui.api import Group, Item, View
+
+
+class SearchList(HasTraits):
+    search_results_list = List(Str, ["No results to display"])
+
+    list_group = Group(
+        Item('_'),
+        Item('search_results_list', style='readonly'),
+        Item('_'),
+    )
+
+    view = View(
+        list_group,
+        title='ListEditor',
+        buttons=['OK'],
+        resizable=True,
+    )
 
 
 if __name__ == "__main__":
@@ -10,4 +28,10 @@ if __name__ == "__main__":
     opts, args = search.parse_args(cmd_args)
 
     results = search.search(args, opts)
-    print(*["{}: {}".format(result['name'], result['version']) for result in results], sep='\n')
+    results_list = [
+        "{}: {}".format(result['name'], result['version'])
+        for result in results
+    ]
+
+    search_list = SearchList(search_results_list=results_list)
+    search_list.configure_traits()
