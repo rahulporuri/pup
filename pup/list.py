89dbce22dbc83612476ca3545bf5a981b2c5315e
Poruri Sai Rahul <rporuri@enthought.com>
2017-09-11 22:14:04 -0500

    ADD : list installed pkgs in UI

---
+++ pup/list.py
@@ -0,0 +1,41 @@
+from pip import parseopts
+from pip.commands.list import ListCommand
+from pip.utils import get_installed_distributions
+
+from traits.api import HasTraits, List, Str
+from traitsui.api import Group, Item, View
+
+
+class InstalledPkgsList(HasTraits):
+    results_list = List(Str, ["No results to display"])
+
+    list_group = Group(
+        Item('_'),
+        Item('results_list', style='readonly'),
+        Item('_'),
+    )
+
+    view = View(
+        list_group,
+        title='ListEditor',
+        buttons=['OK'],
+        resizable=True,
+    )
+
+
+if __name__ == "__main__":
+    cmd_name, cmd_args = parseopts(['list'])
+    list_ = ListCommand()
+    opts, args = list_.parse_args(cmd_args)
+
+    pkgs = get_installed_distributions(
+        local_only=opts.local,
+        user_only=opts.user,
+        editables_only=opts.editable
+    )
+
+    # FIXME : hack for the moment
+    # figure out how to use ListCommand.output_package_listing_*
+    pkgs = [pkg.__str__() for pkg in pkgs]
+    pkg_list = InstalledPkgsList(results_list = list(pkgs))
+    pkg_list.configure_traits()
